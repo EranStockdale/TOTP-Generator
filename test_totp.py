0df2dd3c40ad3d3a7a3da092ff7f1be5fc0013cf
CommandCracker8 <68102222+CommandCracker8@users.noreply.github.com>
2023-06-24 17:49:53 +0100

    make cleaner

--- test_totp.py
+++ test_totp.py
@@ -8,25 +8,22 @@ def to_base32(string: str) -> bytes:
     print(bytearray(encoded_ints).hex())
 
 if __name__ == '__main__':
-    import base64
-    from totp_generator import totp, get_time_steps, get_decimal_time_steps
+    from totp_generator import generate_totp, get_time_steps, get_decimal_time_steps
     from hotp_generator import to_bits
     import math
     time_step_size = 30
 
     key = input("Enter your TOTP key: ")
-    # key = key.encode('ascii')
-    key = base64.b32decode(key.upper() + '=' * ((8 - len(key)) % 8))
     prev_totp = None
     for i in range(get_time_steps(time_step_size) - 100, get_time_steps(time_step_size)):
-        new_totp = totp(key, counter=i - 1)
+        new_totp = generate_totp(key, counter=i - 1)
         if prev_totp != new_totp:
             print(new_totp)
             prev_totp = new_totp
     
     while True:
-        new_totp = totp(key)
+        new_totp = generate_totp(key)
         if prev_totp != new_totp:
             prev_totp = new_totp
             # print(prev_totp)
-        print(totp(key), f"Updating in {math.floor(time_step_size * (1 - (get_decimal_time_steps(time_step_size) % 1))) + 1} second(s)", end=('\r' if prev_totp == new_totp else '\n'))
+        print(new_totp, f"Updating in {math.floor(time_step_size * (1 - (get_decimal_time_steps(time_step_size) % 1))) + 1} second(s)", end=('\r' if prev_totp == new_totp else '\n'))
--- totp_generator.py
+++ totp_generator.py
@@ -1,6 +1,7 @@
 import time
 from hotp_generator import hotp_value
 import math
+import base64
 
 def get_decimal_time_steps(step_size = 30):
     return time.time() / step_size
@@ -8,8 +9,16 @@ def get_decimal_time_steps(step_size = 30):
 def get_time_steps(step_size = 30):
     return math.floor(get_decimal_time_steps(step_size))
 
-def totp(key: bytes, counter = None):
+def totp(key: bytes, counter: int = None):
     if counter == None:
         counter = get_time_steps()
     
     return hotp_value(key, counter.to_bytes(8, 'big'), 6)
+
+def generate_totp(key: str, counter: int = None):
+    if counter == None:
+        counter = get_time_steps()
+
+    key = base64.b32decode(key.upper() + '=' * ((8 - len(key)) % 8))
+    
+    return totp(key, counter)
